5754c1025a7594a58dc568b5aadf207b2f505ada
pw <ashurov.manuchehr@pimpay.ru>
2023-04-06 13:57:35 +0700

    Added ElementCounter class

---
+++ src/ElementCounter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ProgerWolf\PhpArrayElementsCount;
+
+class ElementCounter
+{
+    public function countElements(array $array): int
+    {
+        return count($array);
+    }
+}
